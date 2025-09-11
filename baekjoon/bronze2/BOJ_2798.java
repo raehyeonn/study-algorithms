67d6b951602f08228ea7728af2869664aea55113
raehyeonn <zhhyeon32@naver.com>
2025-09-11 23:30:48 +0900

    feat: 블랙잭(2798번) 문제 풀이

---
+++ baekjoon/bronze2/BOJ_2798.java
@@ -0,0 +1,39 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2798 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+
+                    if (sum <= M) {
+                        max = Math.max(max, sum);
+                    }
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+
+}
