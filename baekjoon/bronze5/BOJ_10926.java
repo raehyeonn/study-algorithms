2b9437f379ac6033158f68dd8a79aa2bf2a31bd1
raehyeonn <zhhyeon32@naver.com>
2025-09-17 17:11:17 +0900

    feat: ??!(10926번) 문제 풀이

---
+++ baekjoon/bronze5/BOJ_10926.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10926 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String id = br.readLine();
+
+        System.out.print(id + "??!");
+    }
+
+}
