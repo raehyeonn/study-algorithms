653b78cce19fd34e44695e2d71352491fd02f49a
raehyeonn <zhhyeon32@naver.com>
2025-10-05 23:44:56 +0900

    feat: 코딩은 체육과목 입니다(25314번) 문제 풀이

---
+++ baekjoon/bronze5/BOJ_25314.java
@@ -0,0 +1,23 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_25314 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N / 4; i++) {
+            sb.append("long ");
+        }
+
+        sb.append("int");
+        System.out.print(sb);
+    }
+
+}
