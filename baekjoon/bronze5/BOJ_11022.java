1a91d1b5b930446364061ed418d43dd7b6922a0e
raehyeonn <zhhyeon32@naver.com>
2025-10-08 21:29:04 +0900

    feat: A+B - 8(11022번) 문제 풀이

---
+++ baekjoon/bronze5/BOJ_11022.java
@@ -0,0 +1,27 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11022 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int sum = A + B;
+
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + sum);
+        }
+    }
+
+}
