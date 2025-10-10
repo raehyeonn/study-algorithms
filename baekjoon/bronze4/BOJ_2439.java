47545ee779843dfe81352e4ffc5c8c86bc815007
raehyeonn <zhhyeon32@naver.com>
2025-10-10 23:27:27 +0900

    feat: 별 찍기 - 2(2439번) 문제 풀이

---
+++ baekjoon/bronze4/BOJ_2439.java
@@ -0,0 +1,23 @@
+package bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2439 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(" ".repeat(N - i));
+            sb.append("*".repeat(i));
+
+            System.out.println(sb);
+        }
+    }
+
+}
