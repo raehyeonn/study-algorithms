196b4b83241f7ab39a8716e029d293eb66f890a4
raehyeonn <zhhyeon32@naver.com>
2025-10-11 17:08:14 +0900

    feat: A+B - 5(10952번) 문제 풀이

---
+++ baekjoon/bronze5/BOJ_10952.java
@@ -0,0 +1,31 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10952 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A + B == 0) {
+                break;
+            }
+
+            sb.append(A + B).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+}
