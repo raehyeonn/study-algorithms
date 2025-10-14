2c3b87ba8828ced2d7eacd2bf4a03300b0b4071b
raehyeonn <zhhyeon32@naver.com>
2025-10-14 23:55:10 +0900

    feat: 공 넣기(10810번) 문제 풀이

---
+++ baekjoon/bronze3/BOJ_10810.java
@@ -0,0 +1,35 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10810 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        for (int a = 0; a < M; a++) {
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int b = i - 1; b < j; b++) {
+                arr[b] = k;
+            }
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
